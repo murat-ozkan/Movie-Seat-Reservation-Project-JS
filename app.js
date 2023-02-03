740a6d2b66c3b23da1ce9753e91f1ff8273c11f5
muratozkan79 <muratozkan79@gmail.com>
2023-02-03 14:06:56 +0300

    some changes

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const seats = document.querySelectorAll(".fa-solid.fa-couch");
+const chosenSeat = [];
+
 for (const seat of seats) {
   seat.addEventListener("click", function () {
+    console.log("start " + this.style.color + this.id);
     if (this.style.color === "") {
       this.style.color = "green";
       console.log(this.id);
+      if (!chosenSeat.includes(this.id)) {
+        chosenSeat.push(this.id);
+        console.log(chosenSeat);
+      }
     } else {
+      chosenSeat.splice(this.id);
       this.style.color = "";
+      console.log(chosenSeat);
     }
   });
 }
 
+const reserveButton = document.querySelector("#reserveButton");
+reserveButton.addEventListener("click", function () {
+  console.log("ara beni");
+});
 // this.style.pointerEvents = 'none';
